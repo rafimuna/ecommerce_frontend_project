4b10fdcee6c1ba136312d85d3b28a7cf23c00b53
rafimuna <rafimuna77@gmail.com>
2025-06-24 21:30:53 +0600

    install axios and pinia

---
+++ src/boot/axios.js
@@ -0,0 +1,12 @@
+// src/boot/axios.js
+import { boot } from 'quasar/wrappers'
+import axios from 'axios'
+
+const api = axios.create({ baseURL: 'http://127.0.0.1:8000/api/' })
+
+export default boot(({ app }) => {
+  app.config.globalProperties.$axios = axios
+  app.config.globalProperties.$api = api
+})
+
+export { api }
